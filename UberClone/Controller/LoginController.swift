6b8e7790eaf8e1af0d76ca8842272aa87024cb6f
mitsuyoshi <m123matsuo@icloud.com>
2020-03-22 20:48:32 +0900

    Project Setup

---
+++ UberClone/Controller/LoginController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginController.swift
+//  UberClone
+//
+//  Created by mitsuyoshi matsuo on 2020/03/22.
+//  Copyright © 2020 mitsuyoshi matsuo. All rights reserved.
+//
+
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
