d6520a04dee87b7810004ad5e43711b61d5b3a84
mitsuyoshi <m123matsuo@icloud.com>
2020-04-03 12:28:15 +0900

    Requesting User Location

--- UberClone/Controller/HomeController.swift
+++ UberClone/Controller/HomeController.swift
@@ -15,12 +15,14 @@ class HomeController: UIViewController {
     // MARK: - Properties
     
     private let mapView = MKMapView()
+    private let locationManager = CLLocationManager()
 
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         checkIfUserIsLoggedIn()
+        enableLocationServices()
 //        signOut()
         view.backgroundColor = .red
     }
@@ -55,10 +57,36 @@ class HomeController: UIViewController {
         view.addSubview(mapView)
         mapView.frame = view.frame
     }
+}
+
+// MARK: - LocationServices
+
+extension HomeController: CLLocationManagerDelegate {
+    func enableLocationServices(){
+        locationManager.delegate = self
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            print("DEBUG: Not determined..")
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            break
+        case .authorizedAlways:
+            print("DEBUG: Auth always..")
+            locationManager.startUpdatingLocation()
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        case .authorizedWhenInUse:
+            print("DEBUG: Auth when in use..")
+            locationManager.requestAlwaysAuthorization()
+        @unknown default:
+            break
+        }
+    }
     
-    
-    // MARK: -
-    // MARK: -
-    // MARK: -
-    // MARK: -
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == .authorizedWhenInUse {
+            locationManager.requestAlwaysAuthorization()
+        }
+    }
 }
