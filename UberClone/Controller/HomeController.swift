55dfc2148c1ed23a38a38c155b18238021016a5b
mitsuyoshi <m123matsuo@icloud.com>
2020-04-04 06:50:56 +0900

    Location TableView Cell + Sections

--- UberClone/Controller/HomeController.swift
+++ UberClone/Controller/HomeController.swift
@@ -108,6 +108,7 @@ class HomeController: UIViewController {
         
         tableView.register(LocationCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.rowHeight = 60
+        tableView.tableFooterView = UIView()
         
         let height = view.frame.height - locationInputViewHeight
         tableView.frame = CGRect(x: 0, y: view.frame.height, width: view.frame.width, height: height)
@@ -163,12 +164,12 @@ extension HomeController: LocationInputActivationViewDelegate {
 extension HomeController: LocationInputViewDelegate {
     func dismissLocationInputView() {
         print("DEBUG: 123")
-        locationInputView.removeFromSuperview()
         
         UIView.animate(withDuration: 0.3, animations: {
             self.locationInputView.alpha = 0
             self.tableView.frame.origin.y = self.view.frame.height
         }) { _ in
+            self.locationInputView.removeFromSuperview()
             UIView.animate(withDuration: 0.3, animations: {
                 self.inputActivationView.alpha = 1
             })
@@ -176,9 +177,17 @@ extension HomeController: LocationInputViewDelegate {
     }
 }
 
+// MARK: - UITableViewDelegate/DataSource
+
 extension HomeController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Test"
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return section == 0 ? 2 : 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
