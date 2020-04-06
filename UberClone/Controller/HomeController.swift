1405c8f27cec04dc4744c509c50ee09971f98308
mitsuyoshi <m123matsuo@icloud.com>
2020-04-07 03:07:32 +0900

    Animating View To Show/Hide

--- UberClone/Controller/HomeController.swift
+++ UberClone/Controller/HomeController.swift
@@ -28,15 +28,16 @@ class HomeController: UIViewController {
     
     private let mapView = MKMapView()
     private let locationManager = LocationHandler.shared.locationManager
-
+    
     private let inputActivationView = LocationInputActivationView()
-
     private let rideActionView = RideActionView()
-
     private let locationInputView = LocationInputView()
     private let tableView = UITableView()
     private var searchResults = [MKPlacemark]()
     private final let locationInputViewHeight: CGFloat = 200
+    
+    private final let rideActionViewHeight: CGFloat = 300
+
     private var actionButtonConfig = ActionButtonConfiguration()
     private var route: MKRoute?
 
@@ -76,6 +77,7 @@ class HomeController: UIViewController {
             UIView.animate(withDuration: 0.3) {
                 self.inputActivationView.alpha = 1
                 self.configureActionButton(config: .showMenu)
+                self.animateRideActionView(shouldShow: false)
             }
             
         }
@@ -205,10 +207,8 @@ class HomeController: UIViewController {
     func configureRideActionView() {
         view.addSubview(rideActionView)
 //        rideActionView.delegate = self
-        rideActionView.frame = CGRect(x: 0, y: view.frame.height - 300,
-                                      width: view.frame.width, height: 300)
-//        rideActionView.frame = CGRect(x: 0, y: view.frame.height,
-//                                      width: view.frame.width, height: rideActionViewHeight)
+        rideActionView.frame = CGRect(x: 0, y: view.frame.height,
+                                      width: view.frame.width, height: rideActionViewHeight)
     }
 
     func configureTableView() {
@@ -231,6 +231,13 @@ class HomeController: UIViewController {
             self.tableView.frame.origin.y = self.view.frame.height
             self.locationInputView.removeFromSuperview()
         }, completion: completion)
+    }
+    
+    func animateRideActionView(shouldShow: Bool) {
+        let yOrigin = shouldShow ? self.view.frame.height - self.rideActionViewHeight : self.view.frame.height
+        UIView.animate(withDuration: 0.3) {
+            self.rideActionView.frame.origin.y = yOrigin
+        }
     }
 }
 
@@ -402,8 +409,9 @@ extension HomeController: UITableViewDelegate, UITableViewDataSource {
             self.mapView.selectAnnotation(annotation, animated: true)
             
             let annotations = self.mapView.annotations.filter({ !$0.isKind(of: DriverAnnotation.self)})
-            
             self.mapView.showAnnotations(annotations, animated: true)
+            
+            self.animateRideActionView(shouldShow: true)
         }
     }
 }
