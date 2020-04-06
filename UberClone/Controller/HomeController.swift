fe0f8de6fafb1435fd560c48ad568993026f4540
mitsuyoshi <m123matsuo@icloud.com>
2020-04-06 22:16:27 +0900

    User Interface Setup

--- UberClone/Controller/HomeController.swift
+++ UberClone/Controller/HomeController.swift
@@ -30,6 +30,9 @@ class HomeController: UIViewController {
     private let locationManager = LocationHandler.shared.locationManager
 
     private let inputActivationView = LocationInputActivationView()
+
+    private let rideActionView = RideActionView()
+
     private let locationInputView = LocationInputView()
     private let tableView = UITableView()
     private var searchResults = [MKPlacemark]()
@@ -156,6 +159,8 @@ class HomeController: UIViewController {
     func configureUI(){
         configureMapView()
         
+        configureRideActionView()
+        
         view.addSubview(actionButton)
         actionButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, paddingTop: 16, paddingLeft: 20, width: 30, height: 30)
         
@@ -197,6 +202,15 @@ class HomeController: UIViewController {
         }
     }
     
+    func configureRideActionView() {
+        view.addSubview(rideActionView)
+//        rideActionView.delegate = self
+        rideActionView.frame = CGRect(x: 0, y: view.frame.height - 300,
+                                      width: view.frame.width, height: 300)
+//        rideActionView.frame = CGRect(x: 0, y: view.frame.height,
+//                                      width: view.frame.width, height: rideActionViewHeight)
+    }
+
     func configureTableView() {
         tableView.delegate = self
         tableView.dataSource = self
